8565df1bb70006971c72ad81bc26d3d523183c6d
DESKTOP-0PTCBCE\NamBN <nguyenvannamtgdd35@gmail.com>
2022-05-19 10:54:16 +0700

    Microservice Project

---
+++ src/BookStore.Domain.Shared/BookType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore
+{
+    // Xác định các BookType có trong bài toán
+    public enum BookType
+    {
+        Undefined,
+        Adventure,
+        Biography,
+        Dystopia,
+        Fantastic,
+        Horror,
+        Science,
+        ScienceFiction,
+        Poety
+    }
+}
---
+++ src/BookStore.Domain/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace BookStore
+{
+    // xác  định thực thể Book kế thừa từ AuditedAggregateRoot có nghĩa là thực thể Book có thể được truy vấn và làm việc trực tiếp
+    public class Book: AuditedAggregateRoot<Guid> // Guid là khóa chính của thực thể Book
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public float Price { get; set; }   
+    }
+}
